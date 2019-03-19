33410610f806f0e8f4a116f37f7c9de52dcacae9
Mike Smiley <msmiley@alumni.rice.edu>
2019-03-19 09:29:15 -0400

    add shadow

--- src/components/VuestroColumnContainer.vue
+++ src/components/VuestroColumnContainer.vue
@@ -17,6 +17,7 @@ export default {
 .vuestro-column-container {
   display: flex;
   flex-grow: 1;
+  box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.1);
 }
 
 /* clear right padding for all but last (right-most) column */
