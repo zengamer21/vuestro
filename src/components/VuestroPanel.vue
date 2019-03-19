dec114dd60ca17d6af0ecbb5a0ef59aa7e930e73
Mike Smiley <msmiley@alumni.rice.edu>
2019-03-19 09:33:13 -0400

    tweak shadows

--- src/components/VuestroPanel.vue
+++ src/components/VuestroPanel.vue
@@ -20,7 +20,7 @@ export default {
 .vuestro-panel {
   background-color: var(--panel-background);
   border-radius: 2px;
-  box-shadow: 0px 1px 2px 0px rgba(0,0,0,0.1);
+  box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.1);
 }
 
 /* border only for dark ui */
