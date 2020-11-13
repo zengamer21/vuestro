404a7ebf2cd62c9be13df80b8b6b5e3bbd937f20
mchenh <mikechen1991@yahoo.com>
2020-11-13 07:55:13 -0800

    Tooltip finished, code commented and polished

--- src/components/charts/VuestroPieChart.vue
+++ src/components/charts/VuestroPieChart.vue
@@ -5,10 +5,7 @@
         :height="height"
         :style="{ transform: `translate(${margin.left}px, ${margin.top}px)` }"
 		>
-		<!--
-		@mousemove="onMouseover">
-        -->
-      <!--PIE-->
+    <!--PIE-->
 	  <g v-for="d in localData" :transform="`translate(${d.pieX},${d.pieY})`">
 		<path v-for="s in processedSeries" :key="s.field"
 			:d="d.arc"
@@ -21,32 +18,34 @@
 		/>		
       </g> 
       <!--TOOLTIP-->
-	  <template v-if="hideTooltip">
+	  <template v-if="showTooltip">
         <vuestro-svg-tooltip :x="toolTipLocation"
 						              	 :x-max="width"
                              :categoryKey="categoryKey"
                              :utc="utc"
                              :series="processedSeries"
-                             :values="localData[0]">
+                             :values="localData[pieIndex]">
         </vuestro-svg-tooltip>
       </template>
     </svg>
   </div>
 </template>
 
-<script>
 
+<script>
 /* global _ */
 import * as d3 from 'd3';
 
 export default {
   name: 'VuestroPieChart',
   
+  //essentially static data
   props: {
-	data: { type: Array, default: () => [] },
+	  data: { type: Array, default: () => [] },
     options: { type: Object, default: () => {} },
   },    
   
+  //essentially dynamic data
   data() {
     return {
       width: 0,
@@ -69,15 +68,23 @@ export default {
       }],
       stacked: false,
       padding: 0.1,
-      hideTooltip: false,
       utc: false,
+      showTooltip: false,
+      pieIndex: 0,
+      toolTipLocation: 0,
+      processedSeries: [{
+        field: "value",
+        title: "Value"
+      }],
     };
   },
   
+
   computed: {
     color() {
       return d3.scaleOrdinal(this.colors);
     },
+    /* No longer called
     // process series prop by adding default colors
     processedSeries() {
       console.log("series");
@@ -98,15 +105,17 @@ export default {
     },
     getCursor() {
       return d3.area().x(d => d.center).y0(this.height).y1(0);
-    },
+    },*/
   },
   
   watch: {
+    //called early in the component lifecycle, so basically initialization is redraw()
     data(newVal) {
       this.redraw();
     },
   },
   
+  //load options data in
   beforeMount() {
     _.merge(this, this.options);
   },
@@ -122,6 +131,7 @@ export default {
   
   //Methods
   methods: {
+    //redraw if window changes
     resize() {
       if (this.$el.clientWidth > 0 && this.$el.clientHeight > 0) {
         this.width = this.$el.clientWidth - this.margin.left - this.margin.right;
@@ -129,66 +139,70 @@ export default {
         this.redraw();
       }
     },
-	
-    getPie(v) {
-    },
-	
+  
+    //redraw componenet
     redraw() {	  
-	  let path = d3.select("path");
+      //clones this.data into dynamic localdata
       this.localData = _.cloneDeep(this.data);
-	  
-	  //Append changing container values to container	  
-	  //instantiate center of pie chart defaults to 150,150
-	  let pieX=0, pieY=0;	  
-	  if(document.getElementById('Pie').getAttribute("height") == 0) {
-	    pieY = 150;
-	  } else {
-		pieY = document.getElementById('Pie').getAttribute("height")/2;	  
-	  }	  
-	  if(document.getElementById('Pie').getAttribute("width") == 0) {
-	    pieX = 150;
-	  } else {
-	    pieX = document.getElementById('Pie').getAttribute("width")/2;
-	  }	  
+      
+      //Append changing container values to container	  
+      //instantiate center of pie chart defaults to 150,150            
+      let pieX=0, pieY=0;	      
+      if(document.getElementById('Pie').getAttribute("height") == 0) {
+        pieY = 150;
+      } else {
+      pieY = document.getElementById('Pie').getAttribute("height")/2;	  
+      }	  
+      if(document.getElementById('Pie').getAttribute("width") == 0) {
+        pieX = 150;
+      } else {
+        pieX = document.getElementById('Pie').getAttribute("width")/2;
+      }	  
 
-	  let pieData = {}
-	  for(let i=0; i<this.localData.length; i++) {
-		pieData[this.localData[i].key] = this.localData[i].value;
-	  }
-	  let pie = d3.pie()
-		.value(function(d) {return d.value;});
-	  let data_ready = pie(d3.entries(pieData));
-	  	  
-	  //Update values of pie chart
-	  for(let i=0; i<this.localData.length; i++) {
-		  //Set pie chart center and radius
-		  this.localData[i]["pieX"] = pieX;
-		  this.localData[i]["pieY"] = pieY;
-		  this.localData[i]["pieRadius"] = pieY-5;		  
-		  
-		  //generate svg path data
-		  let arcGenerator = d3.arc();
-		  let pathData = arcGenerator({
-			startAngle: data_ready[i]["startAngle"],
-			endAngle: data_ready[i]["endAngle"],
-			innerRadius: 0,
-			outerRadius: pieY-5
-		  });
-		  
-		  //set path data
-		  this.localData[i]["arc"] = pathData;
-		  
-		  //set initial color
-		  if(this.localData[i]["color"] === void(0)) {	
-		    this.localData[i]["color"] = this.color(this.localData[i].value);
-		  }		  
-	  }	 	 
-	  
-	  
+      //create path object (section of pie)
+      let path = d3.select("path");
+
+      //create pie data to be ingested
+      let pieData = {}
+      for(let i=0; i<this.localData.length; i++) {
+        pieData[this.localData[i].key] = this.localData[i].value;
+      }
+
+      //create pie object to computer starting/ending angles for pie sections
+      let pie = d3.pie().value(function(d) {return d.value;});
+      let data_ready = pie(d3.entries(pieData));
+          
+      //Update values of pie chart
+      for(let i=0; i<this.localData.length; i++) {
+        //Set pie chart center and radius
+        this.localData[i]["pieX"] = pieX;
+        this.localData[i]["pieY"] = pieY;
+        //margin of 5px
+        this.localData[i]["pieRadius"] = pieY-5;		  
+        
+        //generate svg path data (pie sections)
+        let arcGenerator = d3.arc();
+        let pathData = arcGenerator({
+          startAngle: data_ready[i]["startAngle"],
+          endAngle: data_ready[i]["endAngle"],
+          innerRadius: 0,
+          outerRadius: pieY-5,
+        });
+        
+        //set paths (pie sections) into local data
+        this.localData[i]["arc"] = pathData;
+        
+        //set initial color (do not change after setting)
+        if(this.localData[i]["color"] === void(0)) {	
+          this.localData[i]["color"] = this.color(this.localData[i].value);
+        }		  
+      }	 	 
+      
+      /* UNUSED
       let scaleX = d3.scaleBand()
-                     .domain(this.data.map((d) => { return d[this.categoryKey]; }))
-                     .rangeRound([this.margin.left, this.width - this.margin.right])
-                     .padding(this.padding);
+                    .domain(this.data.map((d) => { return d[this.categoryKey]; }))
+                    .rangeRound([this.margin.left, this.width - this.margin.right])
+                    .padding(this.padding);
 
       let scaleY = d3.scaleLinear().range([this.height, 0]);
       let stackedData;
@@ -220,16 +234,14 @@ export default {
             d.center = scaleX(d[this.categoryKey]) + scaleX.bandwidth() / 2;
           }
         }
-      }
+      }*/
     },
-	
-	//Update tooltip values
+    
+    //Update tooltip values
     onMouseover({ offsetX }) {
       //console.log(event.target);	  
       //console.log(event.target.attributes.d.nodeValue);
-      console.log("THIS");
-      console.log(this);
-      this.toolTipLocation = 200;
+      this.toolTipLocation = event.offsetX;
       //grab selected pie section
       let pieSection = event.target.attributes.d.nodeValue;
           
@@ -237,33 +249,30 @@ export default {
       let pieSectionData;	  
       for(let i=0; i<this.localData.length; i++) {
         if(this.localData[i]["arc"] == pieSection) {
-        pieSectionData = this.localData[i];
-      }
-	  }		 
-	  
-    //Set tool key
-    console.log("update");
-    this.options.series[0].title = pieSectionData.key;
-    this.options.series[0].color = pieSectionData.color;
-    console.log(this.options.series);
-
-	  //Set tool data
-	  let toolValue = [{
-	    "value": pieSectionData.value,
-	  }]
-	  this.data.toolData = toolValue;
-	  
-	  console.log(this.data.toolData);
+          pieSectionData = this.localData[i];
+          this.pieIndex = i;
+        }
+      }		 
+      
+      //THIS MAY STILL BE IMPLEMENTED
+      //ATTEMPT TO CHANGE COLOR ON TOOLTIP BUT THIS IS SET BEFOREHAND SO CAN'T BE CHANGED DYNAMICALLY
+      //this.processedSeries.color = pieSectionData.color;
+      
+      //Set tool data
+      let toolValue = [{
+        "value": pieSectionData.value,
+      }]
+      this.data.toolData = toolValue;    
     },
-	
-	//Hide/show tooltip
-	onMouseenter() {
-	  this.data.hideTooltip = true;	  
-	},
+    //Hide/show tooltip
+    onMouseenter() {
+      this.showTooltip = true; 
+    },
     onMouseexit() {
-	  this.data.hideTooltip = false;	  
-	},
-	
+      this.showTooltip = false;	  
+    },
+  
+    /* UNUSED
     getClosestPoint(x) {
       return this.localData
         .map((point, index) => ({
@@ -275,7 +284,7 @@ export default {
     },
     onMouseleave() {
       this.cursorLine = '';
-    },
+    }, */
   },
 };
 
